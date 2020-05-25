3ac7c098518af80c74cbbdb664968e4339fdcc51
Jason McGloin <jason.mcgloin@gmail.com>
2020-05-25 00:15:15 -0400

    Finish mergeSort. Start quickSort.

--- challenges/mergeSort/mergingArray.js
+++ challenges/mergeSort/mergingArray.js
@@ -1,8 +1,8 @@
-const mergingArrays = (a1, a2) => {
+const mergeArrays = (a1, a2) => {
 	let [i, j] = [0,0];
 	let mergedArray = [];
 	while(i < a1.length && j < a2.length) {
-		if(a1[i] <= a2[j]) {
+		if(a1[i] < a2[j]) {
 			mergedArray.push(a1[i]);
 			++i;
 		} else {
@@ -10,7 +10,18 @@ const mergingArrays = (a1, a2) => {
 			++j;
 		}
 	}
-	return mergedArray;
+	return mergedArray.concat(a1.slice(i)).concat(a2.slice(j));
 }
 
-console.log(mergingArrays([1,5,8],[2,3,6,7]));
+const mergeSort = (array) => {
+	if(array.length > 1) {
+		let half = Math.floor(array.length / 2);
+		let left = mergeSort(array.slice(0,half));
+		let right = mergeSort(array.slice(half));
+		return mergeArrays(left, right);
+	} else {
+		return array;
+	}
+}
+
+console.log(mergeSort([1,5,8,9,2,6,3,7]));
